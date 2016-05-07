f994e24f1d16cb53ac318aea16808e135ae8d9a4
Pavel Gordienko <guzmo6@rambler.ru>
2016-05-07 14:38:10 +0300

    add expect

--- hero_spec.rb
+++ hero_spec.rb
@@ -3,4 +3,9 @@ require './hero'
 
 # тестируем
 describe Hero do 
+  it "has a capitalized name" do
+    hero = Hero.new 'mike'
+    # ожидаем что метод hero.name == Mike
+    expect(hero.name).to eq 'Mike'
+  end
 end
