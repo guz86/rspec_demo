c2e2b69e4ff3473ccb2695ba00a0651c0da03563
Pavel Gordienko <guzmo6@rambler.ru>
2016-05-07 14:38:57 +0300

    add #

--- hero.rb
+++ hero.rb
@@ -23,8 +23,8 @@ class Hero
 
 end
 
-hero = Hero.new 'mike'
-puts hero.name
-puts hero.power_up
-puts hero.power_down
-puts hero.hero_info
+# hero = Hero.new 'mike'
+# puts hero.name
+# puts hero.power_up
+# puts hero.power_down
+# puts hero.hero_info
