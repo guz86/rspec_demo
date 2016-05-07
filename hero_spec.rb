24605a05ca8ba3411ad4e5159eed03d729ed9311
Pavel Gordienko <guzmo6@rambler.ru>
2016-05-07 14:45:03 +0300

    add power_up test

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,13 +1,21 @@
 # подключаем класс
 require './hero'
 
-# тестируем
+# тестируем для name
 describe Hero do 
   it "has a capitalized name" do
     hero = Hero.new 'mike'
     # ожидаем что метод hero.name == Mike
     expect(hero.name).to eq 'Mike'
   end
+
+# тестируем для power_up
+  it "can power up" do
+    hero = Hero.new 'mike'
+
+    expect(hero.power_up).to eq 110
+  end
+
 end
 
 # проверка через rspec hero_spec.rb --color
